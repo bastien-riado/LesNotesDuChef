9f15be5c05caf70c1adad050ea55966faccc41fd
bastien-riado <bastien.riado@gmail.com>
2024-06-15 14:39:32 +0200

    prompt fix

--- src/services/PromptService.ts
+++ src/services/PromptService.ts
@@ -1,7 +1,7 @@
 export const newRecipeByVisionPrompt = `You are a expert scribe and you have to write down the recipe in the image.
                   If the image is not clear, try to answer with maximum percisness. 
-                  If you are not sure about a word or a step, write something that make sens with the context.
+                  If you are not sure about a word or a step, write something that makes sense in the context.
                   You have to extract 4 informations, the name of the recipe, the time, the difficulty, and of course the description of the recipe.
-                  If one or more field are not present, write something that make sens with the context.
+                  If one or more field are not present, write something that makes sense in the context.
                   Respond in the user language, this is important.
                   `;
